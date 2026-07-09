a21f09535f4617d524149d0af34311ebfaa7aaa1
Gleidson <gleidsonrosasantos2@gmail.com>
2026-07-09 16:50:48 -0300

    tem que concertar muita coisa (erros do google e sincronizar as imagens

--- js/produtos.js
+++ js/produtos.js
@@ -1,6 +1,5 @@
 //IMPORTANDO OS PRODUTOS DE /lista_produtos.js//
-import { produtos } from "../lista_produtos.js";
-
+import { produtos } from "./lista_produtos.js";
 //PEGANDO OS ELEMENTOS DO DOM//
 const sectionCards = document.querySelector('#cards')
 
@@ -11,15 +10,15 @@ const lista_produtos = () => {
 
         produtos.array.forEach((elem, i) => {
             
-    const divCards = document.createElement('div')
+    const divCard = document.createElement('div')
     divCards.setAttribute('class', 'card')
 
     const imgCard = document.createElement('img')
-    imgCard.setAttribute('src', elem.caminho.imagem)
-    imgCard.setAttribute('alt', elem.descricao_produtos)
+    imgCard.setAttribute('src', elem.caminho_imagem)
+    imgCard.setAttribute('alt', elem.descricao_produto)
 
     const pCard = document.createAttribute('p')
-    pCard.innerHTML = elem.descricao_produtos
+    pCard.innerHTML = elem.descricao_produto
 
     const h2Card = document.createAttribute('h2')
     h2Card.innerHTML = `R$ ${parseFloat(elem.valor_unitario).toFixed(2).replace('.',',')}`
@@ -28,6 +27,15 @@ const lista_produtos = () => {
     btnCard.setAttribute('class', 'btn-add')
     btnCard.innerHTML = 'adicionar'
 
-});
+     divCard.appendChild(imgCard)
+     divCard.appendChild(pCard)
+     divCard.appendChild(h2Card)
+     divCard.appendChild(btnCard)
+
+     sectionCards.appendChild(divCard)
+
+    });
 
 }
+
+lista_produtos()
