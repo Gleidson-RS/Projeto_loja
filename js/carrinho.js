0b41bfcc105e19af51a491645b2206d18c46d67e
Gleidson <gleidsonrosasantos2@gmail.com>
2026-07-13 17:02:36 -0300

    quase terminando

--- js/carrinho.js
+++ js/carrinho.js
@@ -15,12 +15,48 @@ const addItem = (ObjItem)=>{
 const listItens = () =>{
     const listaItens = JSON.parse(sessionStorage.getItem('carrinhoSessao'))
 
-    listaItens.forEach((elem, i) => {
-        console.log(`elemento ${i=1} - ${elem.descriçao_produto} - ${elem.valorunitario} ${elem.unidade} `)
-        alert(`elemento ${i=1} - ${elem.descriçao_produto} - ${elem.valorunitario} ${elem.unidade} `)
-    })
+    montaTelaCarrinho(listItens)
 }
 
+//MONTAR TELA CARRINHO  
+const montaTelaCarrinho = (objListaItens)=>{
+    const sectionItensCarrinho = document.querySelector('#itens-carrinho')
+    
+    objListaItens.forEach((elem, i) => {
+    const sectionItem = document.createElement('section')
+    sectionItem.setAttribute('class', 'item')
+
+    const divImgItem = document.createElement('div')
+    divImgItem = setAttribute('class', 'img-item')
+
+    const imgIten = document.createElement('img')
+    imgIten.setAttribute('src', elem.caminho_imagem)
+    imgIten.setAttribute('src', elem.descricao_produto)
+
+    const divDescricaoItens = document.createElement('div')
+    divDescricaoItens = setAttribute('class', 'descricoes-itens')
+
+    const divDescricao = document.createElement('div')
+    divDescricao = setAttribute('class', 'valores')
+
+    const divValores = document.createElement('div')
+    divValores.setAttribute('class', 'valores')
+
+    const pIten = document.createElement('p')
+    pIten.innerHTML = `R$ ${parseFloat(elem.valor_unitario).toFixed(2).repeat('.',',')}`
+
+    const inputQuantidade = document.createElement('input')
+    inputQuantidade.setAttribute('type', 'number')
+    inputQuantidade.setAttribute('name', `quant${i}`)
+    inputQuantidade.setAttribute('id', `quant${i}`)
+    inputQuantidade.setAttribute('class', 'input-item')
+    inputQuantidade.setAttribute('value', '1')
+    
+
+    
+    })
+    
+}
 
 
 //EXPORTANDO
