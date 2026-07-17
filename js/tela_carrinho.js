bb3af6643b5d34716b819b3f83242b2f41059b8b
Gleidson <gleidsonrosasantos2@gmail.com>
2026-07-17 13:40:15 -0300

    FUNCIONANDO

--- js/tela_carrinho.js
+++ js/tela_carrinho.js
@@ -51,14 +51,13 @@ const montaTelaCarrinho = () => {
         inputQuantidade.value = elem.quantidade
 
         // Atualiza quantidade sem recarregar a página
-        // ALTERAÇÃO PELO GPT
         inputQuantidade.addEventListener('input', () => {
 
             let quantidade = Number(inputQuantidade.value)
 
             // valida se é inteiro positivo
-            // ALTERAÇÃO PELO GPT
-            if (!Number.isInteger(quantidade) || quantidade <= 0) {
+
+            if (!Number.isInteger(quantidade) || quantidade <= 0) {             // ALTERAÇÃO PELO GPT
 
                 quantidade = 1
                 inputQuantidade.value = 1
@@ -68,7 +67,7 @@ const montaTelaCarrinho = () => {
 
             atualizaResumo()            // atualiza somente os valores da tela
 
-            atualizaValorItem(pCalc, elem.valor_unitario, quantidade)
+            atualizaValorItem(pCalc, elem.valor_unitario, quantidade) //atualiza o valor do item + a quantidade do item
 
 })
 
@@ -80,27 +79,14 @@ const montaTelaCarrinho = () => {
         // valor total do produto
         const pCalc = document.createElement('p')
 
-        pCalc.innerHTML =
-            `R$ ${(elem.valor_unitario * elem.quantidade)
-                .toFixed(2)
-                .replace('.', ',')}`
-
-
-
+        pCalc.innerHTML = `R$ ${(elem.valor_unitario * elem.quantidade).toFixed(2).replace('.', ',')}` //Exibe o valor + quantidade do produto
 
         const imgRemover = document.createElement('img')
 
-        imgRemover.setAttribute(
-            'src',
-            '../imagens/icones/remover.png'
-        )
-
-
+        imgRemover.setAttribute('src', '../imagens/icones/remover.png')
         imgRemover.addEventListener('click', () => {
 
-
-            if (confirm(`tem certeza que deseja remover ${elem.descricao_produto}`)) {
-
+            if (confirm(`tem certeza que deseja remover ${elem.descricao_produto}?`)) {
 
                 removeItemTela(i)
 
@@ -124,57 +110,45 @@ const montaTelaCarrinho = () => {
 
 }
 
-// ALTERAÇÃO
+
 // Atualiza o valor do item individual
 const atualizaValorItem = (elemento, preco, quantidade) => {
 
 
     elemento.innerHTML =
-        `R$ ${(preco * quantidade)
-            .toFixed(2)
-            .replace('.', ',')}`
+        `R$ ${ (preco * quantidade) .toFixed(2).replace('.', ',')}`            // ALTERAÇÃO PELO GPT
 
 }
 
-montaTelaCarrinho()
+montaTelaCarrinho() //atualiza a pagina
+
 
 const removeItemTela = (i) => {
 
     removeItem(i)
-
     montaTelaCarrinho()
-
     atualizaResumo()
 
 }
 
+
 // ATUALIZA O RESUMO DA COMPRA
-
 const atualizaResumo = () => {
-
-
     const itens = listItens() || []
-
-
 
     const total = itens.reduce((soma, item) => {
 
-
-        return soma +
-            (item.valor_unitario * item.quantidade)
-
+        return soma + (item.valor_unitario * item.quantidade)
 
     }, 0)
 
-
-
     document.querySelector('#valor-produtos').innerHTML = `R$ ${total.toFixed(2).replace('.', ',')}`
-    
     
     document.querySelector('#valor-total').innerHTML = `R$ ${total.toFixed(2).replace('.', ',')}`
 
 }
 
+atualizaResumo() //atualiza o resumo
 
 
-atualizaResumo()
+
